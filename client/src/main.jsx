756f21f33bd8670966b1fe52b16460caa1db2f28
Serve-er <120189782+serv-er@users.noreply.github.com>
2024-01-25 02:52:54 +0530

    Eazy-pg

---
+++ client/src/main.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Auth0Provider
+     domain="dev-lf8lrtz6wsvj2n5o.us.auth0.com"
+     clientId="wnmLytGDf0APHLje9oKYJnr2LtXDe3kc"
+     authorizationParams={{
+      redirect_uri: "http://localhost:5173"
+     }}
+     audience="http://localhost:8000"
+     scope="openid profile email"
+    >
+      <App />
+    </Auth0Provider>
+  </React.StrictMode>
+);
